4ca5b4d212a9d6d3ae59d2bc4c44a46e305684cb
ANKITA GUPTA <141404641+ankita430@users.noreply.github.com>
2025-06-26 14:34:26 +0530

    Create Miles to Kilometers Converter.C

---
+++ Miles to Kilometers Converter.c
@@ -0,0 +1,12 @@
+# Miles to Kilometers Converter
+'''
+#include <stdio.h>
+
+int main(void)
+{
+    float miles, km;            // Declaring variables to store miles and kilometers
+    miles = 10.0;               // Assigning 10.0 miles
+    km = miles * 1.609;         // 1 mile = 1.609 kilometers
+    printf("%.2f miles = %.2f kilometers\n", miles, km);  // Print result with 2 decimal points
+    return 0;
+}
